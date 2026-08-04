3f7dcb21a1ccfebc436d33da36d8d9083040dd41
LeninMiranda <lenin9073@gmail.com>
2026-08-03 19:51:08 -0700

    put text, resolution and flip frame

--- src/main.py
+++ src/main.py
@@ -5,9 +5,25 @@ camera = cv2.VideoCapture(0)
 if not camera.isOpened():
     raise RuntimeError("No se pudo abrir la camara")
 
+
+
 try: 
     while True:
         ok, frame = camera.read()
+
+        height, width, channels = frame.shape
+
+        frame = cv2.flip(frame, 1)
+
+        cv2.putText(
+            frame,
+            f"Resolucion: {width} x {height}",
+            (20, 40),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 255, 0),
+            2,
+        )
 
         if not ok:
             print("No se pudo leer el fotograma")
@@ -16,6 +32,7 @@ try:
         cv2.imshow("Mi camara", frame)
 
         key = cv2.waitKey(1) & 0xFF
+
 
         if key == ord("q"):
             break

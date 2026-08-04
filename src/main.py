5ca4c75d622bd53c55bdb4c13add713e56855fb1
LeninMiranda <lenin9073@gmail.com>
2026-08-03 19:45:15 -0700

    camera open

--- src/main.py
+++ src/main.py
@@ -1,2 +1,24 @@
-"""Aquí escribirás la aplicación durante las lecciones."""
+import cv2
 
+camera = cv2.VideoCapture(0)
+
+if not camera.isOpened():
+    raise RuntimeError("No se pudo abrir la camara")
+
+try: 
+    while True:
+        ok, frame = camera.read()
+
+        if not ok:
+            print("No se pudo leer el fotograma")
+            break
+
+        cv2.imshow("Mi camara", frame)
+
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord("q"):
+            break
+finally:
+    camera.release()
+    cv2.destroyAllWindows()
